bb5b4529a151a4559601881b41e8e9f2ab688a55
Katariniss <echekina@gmail.com>
2019-06-10 19:15:10 +0500

    Hides 'see more' button when there are no more hidden ads

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 (function () {
+  const NUMBER_OF_ADS_TO_SHOW = 6;
+
   const seeMoreButton = document.querySelector('.ads__button');
 
   seeMoreButton.addEventListener('click', function () {
     const hiddenAds = document.querySelectorAll('.ads__item--hidden');
     const hiddenAdsArray = Array.from(hiddenAds);
 
-    const firstSixHiddenAds = hiddenAdsArray.slice(0, 6);
+    const firstSixHiddenAds = hiddenAdsArray.slice(0, NUMBER_OF_ADS_TO_SHOW);
 
     for (let i = 0; i < firstSixHiddenAds.length; i++) {
       const hiddenAd = firstSixHiddenAds[i];
 
       hiddenAd.classList.remove('ads__item--hidden');
+    }
+
+    if (hiddenAdsArray.length <= NUMBER_OF_ADS_TO_SHOW) {
+      seeMoreButton.classList.add('ads__button--hidden');
     }
   });
 })();
